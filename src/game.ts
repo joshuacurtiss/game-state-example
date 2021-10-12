a883c71cd333b7793c8eb2c9fb20a1bc57f61a9d
Josh Curtiss <J.Curtiss@rockvalleycollege.edu>
2021-10-11 21:53:29 -0500

    Enable real-time replay of entire history.

--- src/game.ts
+++ src/game.ts
@@ -5,7 +5,8 @@ const {add, debug, keyPress, pos, sprite, scale, vec2, width, height} = k
 
 export default function () {
     const hist: State[] = [new State(0, 0)]
-    let histpos: number
+    let histpos = 0
+    let replaying = false
     // Add sprite
     const mario = add([
         sprite('mario'),
@@ -13,6 +14,7 @@ export default function () {
         pos(0,0),
     ])
     const move = (x: number, y: number) => {
+        if( replaying ) return
         const {x: lastX, y: lastY} = hist[hist.length-1]
         const newpos = vec2(lastX, lastY).add(vec2(x, y))
         if( newpos.x > width() || newpos.x < 0 ) return
@@ -31,12 +33,30 @@ export default function () {
         debug.log(`Replay ${pos+1}: ${state.toString()}`)
         histpos = pos
     }
+    const replayNext = (pos = histpos+1) => {
+        replay(pos)
+        const currstate = hist[histpos]
+        const nextstate = hist[histpos+1]
+        if( !currstate || !nextstate ) {
+            replaying = false
+            return
+        }
+        const timediff = nextstate.dt - currstate.dt
+        setTimeout(replayNext, timediff)
+    }
+    const replayAll = () => {
+        if( replaying ) return
+        replaying = true
+        replayNext(0)
+    }
+    keyPress('enter', replayAll)
     keyPress('up', ()=>move(0,-32))
     keyPress('down', ()=>move(0,32))
     keyPress('left', ()=>move(-32,0))
     keyPress('right', ()=>move(32,0))
-    keyPress(',', ()=>replay(histpos-1))
-    keyPress('.', ()=>replay(histpos+1))
+    keyPress(',', ()=>!replaying ? replay(histpos-1) : false)
+    keyPress('.', ()=>!replaying ? replay(histpos+1) : false)
     debug.log("Use arrow keys to move.")
     debug.log("Use < and > to traverse history.")
+    debug.log("Use 'enter' to replay history in real-time.")
 }
